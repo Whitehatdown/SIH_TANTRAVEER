0a4834afa257429852073753a55bacb201f0a2ac
thedemonsid <128967580+thedemonsid@users.noreply.github.com>
2023-09-24 01:27:34 +0530

    Delete sih directory

--- sih/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    // Animate welcome screen text
-    setTimeout(function () {
-        $("#banner h1").fadeOut(1500);
-        $("#banner h1").slideDown(1500);
-    }, 1500);
-    });
